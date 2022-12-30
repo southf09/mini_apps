0492cd3f4f8de1f4239e0f7ea855e4c190aaaa0f
Frank South <frankstonesouth@gmail.com>
2022-12-30 15:45:53 -0500

    Security changes made to source code

--- weather_api/app.js
+++ weather_api/app.js
@@ -3,20 +3,23 @@ const https = require('https');
 const app = express();
 
 app.get('/', function(req, res){
-    const url = 'https://api.openweathermap.org/data/2.5/weather?lat=35.7803977&lon=-78.6390989&appid=7df43b813165f5a75e08251bb0dd7994&units=imperial';
+    const apiKey = yourKeyHere;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=35.7803977&lon=-78.6390989&appid=${apiKey}&units=imperial`;
+
     https.get(url, function(response){
         console.log(response.statusCode);
+        
+        response.on('data', function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const description = weatherData.weather[0].description;
+            const weatherIcon = weatherData.weather[0].icon;
+            const iconURL = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+
+            res.write(`<h3>The weather is currently ${description}.</h3>`);
+            res.write(`<h1>The temperature in Raleigh is ${temp} degrees fahrenheiht.</h1>`);
+            res.write(`<img src="${iconURL}" alt="" />`)
+            res.send();
+        });
     });
-
-    res.send('Server is up and running.');
-})
-
-
-
-
-
-
-
-app.listen(3000, function() {
-    console.log('Server is running on port 3000.');
 })
