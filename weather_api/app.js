8abdfd1718a2a65a3a24dd87116f260c19dbda5a
Frank South <frankstonesouth@gmail.com>
2023-01-05 14:57:30 -0500

    Added comments explaining the function and process

--- weather_api/app.js
+++ weather_api/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const https = require('https');
 const app = express();
 
+//This function used the openweathermap API to generate data based on specified location. It then writes the data to the root location ('/') at localhost:3000
+
 app.get('/', function(req, res){
     const apiKey = yourKeyHere;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=35.7803977&lon=-78.6390989&appid=${apiKey}&units=imperial`;
